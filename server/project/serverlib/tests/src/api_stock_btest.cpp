70759d39fdcde793d97b239611607414ec9b385c
VAVASTO <vladimir.polotsk@gmail.com>
2023-05-04 23:51:01 +0300

    Added tests

--- server/project/serverlib/tests/src/api_stock_btest.cpp
+++ server/project/serverlib/tests/src/api_stock_btest.cpp
@@ -1,5 +1,26 @@
-#include "api_model.h"
+#include "api_stock.h"
 #include "http_protocol.h"
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <string>
+
+TEST(APIStockRequestTest, GetDataReturnsResponse) {
+    api::APIStockRequest apii;
+    auto response = apii.getData("/test");
+    EXPECT_NE(response, nullptr);
+}
+
+TEST(APIStockRequestTest, GetDataEmptyPath) {
+    api::APIStockRequest api;
+    auto response = api.getData("error_path");
+    std::vector<char> empty_body; 
+    EXPECT_EQ(response->getBoby(), empty_body);
+}
+
+TEST(APIStockRequestTest, GetDataReturnsValidContentType) {
+    api::APIStockRequest apii;
+    auto request = apii.getData("/test");
+    std::map<std::string, std::string> req; 
+    req.insert( std::make_pair("Content-Type", "application/json"));
+    EXPECT_EQ(request->getHeaders(), req);
+}

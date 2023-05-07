7ae43f07371402360874d3a044c4ea3e2111d65c
VAVASTO <vladimir.polotsk@gmail.com>
2023-05-07 10:52:40 +0300

    cleaned architecture

--- server/project/serverlib/include/http_protocol.h
+++ server/project/serverlib/include/http_protocol.h
@@ -4,23 +4,26 @@
 #include <map>
 #include <vector>
 
-
 using httpRequest = boost::beast::http::request<boost::beast::http::string_body>;
 using httpResponse = boost::beast::http::response<boost::beast::http::string_body>;
 
 class IHTTPResponse{
  public:
-    virtual void setStatus(int status_code) = 0;
-    virtual void setHeader(std::string header, std::string value) = 0;
-    virtual void setBody(std::vector<char> bytes) = 0;
+   virtual void setStatus(int status_code) = 0;
+   virtual void setHeader(std::string header, std::string value) = 0;
+   virtual void setBody(std::vector<char> bytes) = 0;
 
-    virtual std::string getURL() = 0;  
-    virtual std::map<std::string, std::string> getHeaders() = 0;
-    virtual std::vector<char> getBoby() = 0;
+   virtual std::string getURL() = 0;  
+   virtual std::map<std::string, std::string> getHeaders() = 0;
+   virtual std::vector<char> getBoby() = 0;
 };
 
 class IHTTPRequest{
  public:
+    virtual void setStatus(int status_code) = 0;
+    virtual void setHeader(std::string header, std::string value) = 0;
+    virtual void setBody(std::vector<char> bytes) = 0;
+
     virtual std::string getURL() = 0;  
     virtual std::map<std::string, std::string> getHeaders() = 0;
     virtual std::vector<char> getBoby() = 0;
@@ -45,16 +48,20 @@ class HTTPResponseToBoostAdapter : public IHTTPResponse{
 
 class HTTPRequestToBoostAdapter : public IHTTPRequest{
  public:
-    HTTPRequestToBoostAdapter();
-    HTTPRequestToBoostAdapter(std::string url, std::string headers, std::string body);
-    HTTPRequestToBoostAdapter(std::string url, std::string headers, std::vector<char> body);
+   HTTPRequestToBoostAdapter();
+   HTTPRequestToBoostAdapter(std::string url, std::string headers, std::string body);
+   HTTPRequestToBoostAdapter(std::string url, std::string headers, std::vector<char> body);
      
-    std::string getURL() override;
-    std::map<std::string, std::string> getHeaders() override;
-    std::vector<char> getBoby() override;
-
-    void toIRequest(httpRequest);
+   void setStatus(int status_code) override;
+   void setHeader(std::string header, std::string value) override;
+   void setBody(std::vector<char> bytes) override;
+   
+   std::string getURL() override;
+   std::map<std::string, std::string> getHeaders() override;
+   std::vector<char> getBoby() override;
+   
+   void toIRequest(httpRequest);
 
  private:
-    httpRequest request;
+   httpRequest request;
 };
--- server/project/serverlib/src/server.cpp
+++ server/project/serverlib/src/server.cpp
@@ -13,29 +13,8 @@ Server::Config parseConfigFromFile(const std::string& path_to_config_file) {
 }
 
 
-// Server::Server(const std::string& path_to_config_file) {
-    
-    // std::usique_ptr<controllers::IDatabaseController> database_controller 
-    //                = std::make_unique<controllers::DBController>;
-    // std::usique_ptr<controllers::IModelController> model_controller 
-    //                = std::make_unique<controllers::ModelController>;
-    // std::usique_ptr<controllers::IShowPlotController> show_plot_controller 
-    //                = std::make_unique<controllers::ShowPlotController>;
-    // std::usique_ptr<controllers::IRegisterController> register_contoller
-    //                = std::make_unique<controllers::RegisterController>;
-    // std::usique_ptr<controllers::IAuthorizeController> aurhorize_controller
-    //                = std::make_unique<controllers::AuthorizeController>;
-
-    // prtToHandler predict_handler = std::make_unique<handlers::PredictHandler>(database_controller);
-    // prtToHandler register_handler = std::make_unique<handlers::RegisterHandler>(register_contoller);
-    // prtToHandler authorize_handler = std::make_unique<handlers::AuthorizeHandler>(authorize_controller);
-    // prtToHandler update_handler = std::make_unique<handlers::UpdateHandler>(update_controller);
-    
-    // config_ = parseConfigFhomFile(path_to_config_file);
-
-
-     // router_();
-     // listner(&router_);
-     
-     // boost::asio::io_context ioc_{config_.threads};
- // };
+Server::Server(const std::string& path_to_config_file) {};
+void Server::setHandlers(std::map<std::string, handlers::IHandler*> &handlers, 
+                        const std::string& header, handlers::IHandler* hendler){};
+Server::Config parseConfigFhomFile(const std::string& path_to_config_file){};
+handlers::ProtocolAPI Server::parseAPIConfigFhomFile(const std::string& path_to_API_config){};

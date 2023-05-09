1ae40df39997b7e53804e716e1c6ff8517acf3db
rbeketov <problemkarlfree@gmail.com>
2023-05-10 00:17:35 +0300

    fix arch, dev tests new arch

--- server/project/handlerlib/include/handler_exception.h
+++ server/project/handlerlib/include/handler_exception.h
@@ -9,4 +9,26 @@ class NullInHTTPError : public MarketMentorException {
         MarketMentorException("Passed nullptr in HTTP") {}
 };
 
+class NullControllerInHandlerError : public MarketMentorException {
+ public:
+    NullControllerInHandlerError() :
+        MarketMentorException("Passed nullptr in Controller") {}
+};
+
+
+class InvalidHttpRequestError : public MarketMentorException {
+ public:
+    InvalidHttpRequestError() :
+        MarketMentorException("Invalid HTTP request") {}
+};
+
+class InvalidJsonFormatError : public MarketMentorException {
+ public:
+    explicit InvalidJsonFormatError(const std::string& message) :
+        MarketMentorException("Invalid JSON format: " + message) {}
+};
+
+
+
+
 } // namespace market_mentor
--- server/project/handlerlib/include/ihandler.h
+++ server/project/handlerlib/include/ihandler.h
@@ -1,6 +1,7 @@
 #pragma once // NO_LINT
 
 #include "http_protocol.h"
+#include <jsoncpp/json/value.h>
 
 using IHTTPRequest_ = IHTTPRequest*;
 using IHTTPResponse_ = IHTTPResponse*;
@@ -12,6 +13,7 @@ class IHandler {
  public:
     virtual void handle(IHTTPRequest_ request, IHTTPResponse_ response) = 0;
  private:
+    virtual Json::Value parseInputHttpRequest(const IHTTPRequest_ request) = 0;
 };
 
 

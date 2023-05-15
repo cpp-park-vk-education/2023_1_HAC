fee6715aa7c5248ac286eb586422291c91cd0364
VAVASTO <vladimir.polotsk@gmail.com>
2023-05-15 18:50:12 +0300

    listening server done

--- server/project/serverlib/include/listener.h
+++ server/project/serverlib/include/listener.h
@@ -5,6 +5,7 @@
 #include <boost/asio/dispatch.hpp>
 #include <boost/asio/strand.hpp>
 #include <boost/config.hpp>
+#include <iostream>
 
 #include "session.h"
 #include "routers.h"
@@ -19,17 +20,22 @@ using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 //     virtual void Run() = 0;
 // };
 
-class Listener : std::enable_shared_from_this<Listener>{
- public:
-    Listener() = delete;
-    Listener(net::io_context& ioc, tcp::endpoint endpoints);
-    void Run();
+// Accepts incoming connections and launches the sessions
+class Listener : public std::enable_shared_from_this<Listener>
+{
+
+public:
+    Listener(
+        net::io_context& ioc,
+        tcp::endpoint endpoint,
+        std::shared_ptr<std::string const> const& doc_root);
+    void run();
+
+private:
+    void do_accept();
+    void on_accept(beast::error_code ec, tcp::socket socket);
     
- //private:
     net::io_context& ioc_;
     tcp::acceptor acceptor_;
-    RouterAdapter* router_adapter;
-
-    void doAccept();
-    void onAccept(beast::error_code ec, tcp::socket socket);
+    std::shared_ptr<std::string const> doc_root_;
 };

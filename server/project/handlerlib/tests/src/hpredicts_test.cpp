242e7f0e523c661c91730287ec04bd2fb3990118
rbeketov <problemkarlfree@gmail.com>
2023-05-21 01:11:30 +0300

    handlepredict test

---
+++ server/project/handlerlib/tests/src/hpredicts_test.cpp
@@ -0,0 +1,138 @@
+// external dependencies
+#include "utils_gtest.h"
+#include "gmock/gmock.h"
+#include <jsoncpp/json/json.h>
+
+#include <memory>
+// internal dependencies
+
+#include "icontrollers.h"
+#include "dbcontroller.hpp"
+#include "handler_exception.h"
+#include "handlers.h"
+#include "controllers.h"
+
+//
+#include "api_model.h"
+#include "http_protocol.h"
+
+
+
+using ::testing::Return;
+using ::testing::Throw;
+using ::testing::_;
+
+
+class MockDataBaseController : public dbcontroller::IDataBaseController {
+public:
+    MOCK_METHOD(Json::Value, DataRequest, (const Json::Value& request), (override));
+    MOCK_METHOD(bool, ConnectToDatabase, (), (override));
+    MOCK_METHOD(void, SetDatabaseConfig, (const std::string&  addr, const std::string&  port,
+        const std::string& db_name, const std::string&  user, const std::string&  pass), (override));
+
+    MOCK_METHOD( Json::Value, TimeSeriesPost, (const Json::Value& data), (override));
+    MOCK_METHOD(Json::Value, TimeSeriesGet, (const std::string& name_stock, const std::string& len_lags), (override));
+
+    MOCK_METHOD(Json::Value, ClientRequestPost, (const Json::Value& data), (override));
+    MOCK_METHOD(Json::Value, ClientRequestGet, (const std::string& key), (override));
+    MOCK_METHOD(Json::Value, ClientRequestUpdate, (const Json::Value& data), (override));
+};
+
+
+// Мок класса IPredictController
+class MockPredictController : public controllers::IPredictController {
+public:
+   MOCK_METHOD(Json::Value, makePredict, (const Json::Value& request), (override));
+   MOCK_METHOD(controllers::TimeSeriesPredicts, makeTimeSeries, (const std::vector<double>& samples_data, size_t window_size), (override));
+   MOCK_METHOD(std::vector<double>, parseDBProtocol, (const Json::Value& response), (override));
+};
+
+
+class MockHTTPRequest: public IHTTPRequest {
+public:
+    MOCK_METHOD(void, setStatus, (int status_code), (override));
+    MOCK_METHOD(void, setUrl, (const std::string& url), (override));
+    MOCK_METHOD(void, setHeader, (const std::string &header, const std::string &value), (override));
+    MOCK_METHOD(void, setBody, (const std::string &bytes), (override));
+    MOCK_METHOD(std::string, getURL, (), (override));
+    MOCK_METHOD((std::map<std::string, std::string>), getHeaders, (), (override));
+    MOCK_METHOD(std::string, getBoby, (), (override));
+};
+
+class MockHTTPResponse: public IHTTPResponse {
+public:
+    MOCK_METHOD(void, setStatus, (int status_code), (override));
+    MOCK_METHOD(void, setHeader, (const std::string &header, const std::string &value), (override));
+    MOCK_METHOD(void, setBody, (const std::string &bytes), (override));
+    MOCK_METHOD(std::string, getURL, (), (override));
+    MOCK_METHOD(int, getStatus, (), (override));
+    MOCK_METHOD((std::map<std::string, std::string>), getHeaders, (), (override));
+    MOCK_METHOD(std::string, getBoby, (), (override));
+    
+};
+
+
+// // Мок класса IModelController
+// class MockModelController : public controllers::IModelController {
+// public:
+//     MOCK_METHOD(callModelApi, Json::Value(const TimeSeriesPredicts&));
+// };
+
+// // Мок класса IShowPlotController
+// class MockShowPlotController : public controllers::IShowPlotController {
+// public:
+//     MOCK_METHOD(createPlotData, Json::Value(const Json::Value&));
+// };
+
+// // Мок класса IRegisterController
+// class MockRegisterController : public controllers::IRegisterController {
+// public:
+//     MOCK_METHOD(registration, Json::Value(Json::Value&));
+//     MOCK_METHOD(makeDBProtocol, Json::Value(const Json::Value&));
+// };
+
+// // Мок класса IAuthorizeController
+// class MockAuthorizeController : public controllers::IAuthorizeController {
+// public:
+//     MOCK_METHOD(authorization, Json::Value(Json::Value&));
+//     MOCK_METHOD(makeDBProtocol, Json::Value(const Json::Value&));
+//     MOCK_METHOD(checkPassword, Json::Value(const Json::Value&, const Json::Value&));
+// };
+
+// // Мок класса IUpdateDataController
+// class MockUpdateDataController : public controllers::IUpdateDataController {
+// public:
+//     MOCK_METHOD(updateData, bool(const handlers::ProtocolAPI&));
+//     MOCK_METHOD(makeDBProtocol, Json::Value(const Json::Value&));
+// };
+
+class PredictHandlerTest: public ::testing::Test {
+public:
+    PredictHandlerTest()
+        : ptr_db_controller(new MockDataBaseController),
+          ptr_predict_controller(new MockPredictController),
+          http_request(new MockHTTPRequest),
+          http_response(new MockHTTPResponse) {}
+protected:
+    MockDataBaseController* ptr_db_controller;
+    MockPredictController* ptr_predict_controller;
+    MockHTTPRequest* http_request;
+    MockHTTPResponse* http_response;
+};
+
+
+
+// TESTS
+TEST_F(PredictHandlerTest, BaseTest) {
+    handlers::PredictHandler predict_handler(ptr_predict_controller);
+
+    
+
+    EXPECT_CALL(ptr_predict_controller, makePredict()).WillOnce(
+        Return(Json::Value{{"status", true}, {"data", Json::Value{{"0", 1} {"1", 1} {"2", 1} {"3", 1}}}})
+    );
+
+    predict_handler.handle()
+    EXPECT_EQ(1, 1);
+}
+
